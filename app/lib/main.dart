8543d4a4aebf75c847c871bda52382166aba1799
rasla <programmer.rasla@gmail.com>
2026-06-13 10:46:25 +0530

    a reset 😄

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,20 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-import 'game/overlays/game_over_overlay.dart';
-import 'game/overlays/main_menu_overlay.dart';
-import 'game/overlays/pause_overlay.dart';
-import 'game/overlays/settings_overlay.dart';
-import 'game/fitit_game.dart';
-
 void main() {
-  runApp(
-    GameWidget<FititGame>.controlled(
-      gameFactory: FititGame.new,
-      overlayBuilderMap: {
-        'MainMenu': (_, game) => MainMenuOverlay(game: game),
-        'GameOver': (_, game) => GameOverOverlay(game: game),
-        'Pause': (_, game) => PauseOverlay(game: game),
-        'Settings': (_, game) => SettingsOverlay(game: game),
-      },
-      initialActiveOverlays: const ['MainMenu'],
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(),
+      ),
+    );
+  }
 }
--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,27 +1,12 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:app/game/fitit_game.dart';
-
-Widget buildGame() {
-  return GameWidget<FititGame>.controlled(
-    gameFactory: FititGame.new,
-    overlayBuilderMap: {
-      'MainMenu': (_, game) => const SizedBox.shrink(),
-      'GameOver': (_, game) => const SizedBox.shrink(),
-      'Pause': (_, game) => const SizedBox.shrink(),
-    },
-    initialActiveOverlays: const ['MainMenu'],
-  );
-}
+import 'package:app/main.dart';
 
 void main() {
-  testWidgets('Game loads without error', (WidgetTester tester) async {
-    SharedPreferences.setMockInitialValues({});
-    await tester.pumpWidget(buildGame());
-    await tester.pump();
-    expect(tester.takeException(), isNull);
+  testWidgets('App loads blank screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.byType(Scaffold), findsOneWidget);
+    expect(find.byType(Container), findsOneWidget);
   });
 }
